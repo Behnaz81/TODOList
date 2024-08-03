f616b480bb62b9e56d89c5c8aae36d694f9cef7f
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-03 12:23:30 +0330

    Connecting to database

--- TODOList/settings.py
+++ TODOList/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'TODOList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': '3412130*D',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
