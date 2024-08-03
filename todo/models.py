d14a51c0358b5ceb4e4c6f9b54992408b15bb6d1
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-03 13:02:15 +0330

    Task model was defined.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    class Priority(models.IntegerChoices):
+        VERY_HIGH = 1
+        HIGH = 2
+        MEDIUM = 3
+        LOW = 4
+        VERY_LOW = 5
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300, null=True, blank=True)
+    priority = models.IntegerField(choices=Priority)
+    is_done = models.BooleanField(default=False)
+
