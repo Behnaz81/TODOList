0e41f624517a2e1ec8ac73ee7ba061815a5f7419
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-04 17:33:26 +0330

    Done function was added to change is_done to True in views.py

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from django.http import HttpResponse
 
 
 # Create your views here.
 def Index(request):
-    tasks_notcompleted = Task.objects.filter(is_done=False)
-    tasks_completed = Task.objects.filter(is_done=True)
+    tasks_notcompleted = Task.objects.filter(is_done=False).order_by("priority")
+    tasks_completed = Task.objects.filter(is_done=True).order_by("priority")
     context = {
         'tasks_notcompleted': tasks_notcompleted,
         'tasks_completed': tasks_completed,
     }
     return render(request, 'index.html', context)
+
+
+def Done(request, id):
+    task = Task.objects.get(id=id)
+    task.is_done = True
+    task.save()
+    return redirect(Index)
