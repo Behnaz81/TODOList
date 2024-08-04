781f5f0af4e30c5b657ed8cf01214ae6efbf6644
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-04 17:50:28 +0330

    url was defined to redirect to delete view in urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import Index
 from .views import Done
+from .views import Delete
 
 
 urlpatterns = [
     path('', Index),
     path('done/<int:id>/', Done, name='done'),
+    path('delete/<int:id>/', Delete, name='delete')
 ]
