d4362bd33dbc1b100f9ea78e585d51a7dd00e15d
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-04 14:43:18 +0330

    divided task into two groups: completed and not completed in vies.py

--- todo/views.py
+++ todo/views.py
@@ -4,8 +4,10 @@ from .models import Task
 
 # Create your views here.
 def Index(request):
-    tasks = Task.objects.all()
+    tasks_notcompleted = Task.objects.filter(is_done=False)
+    tasks_completed = Task.objects.filter(is_done=True)
     context = {
-        'tasks': tasks,
+        'tasks_notcompleted': tasks_notcompleted,
+        'tasks_completed': tasks_completed,
     }
     return render(request, 'index.html', context)
