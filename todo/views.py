b08d1a1613fd903841f55212a7d82edbf6386fa5
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-04 16:48:35 +0330

    Sorting the tasks by priority in view.py

--- todo/views.py
+++ todo/views.py
@@ -4,8 +4,8 @@ from .models import Task
 
 # Create your views here.
 def Index(request):
-    tasks_notcompleted = Task.objects.filter(is_done=False)
-    tasks_completed = Task.objects.filter(is_done=True)
+    tasks_notcompleted = Task.objects.filter(is_done=False).order_by("priority")
+    tasks_completed = Task.objects.filter(is_done=True).order_by("priority")
     context = {
         'tasks_notcompleted': tasks_notcompleted,
         'tasks_completed': tasks_completed,
