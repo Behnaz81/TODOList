6cd5f304f8790e5592c830d5a357b16c51444517
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-04 17:34:19 +0330

    url was added to redirect to the view function in urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import Index
+from .views import Done
 
 
 urlpatterns = [
-    path('', Index)
+    path('', Index),
+    path('done/<int:id>/', Done, name='done'),
 ]
