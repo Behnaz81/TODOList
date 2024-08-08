374921a3339431480747e65bc132e79a0c998ae5
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-08 20:30:03 +0330

    updated index function to work with the forms

--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
-from django.http import HttpResponse
+
 
 
 # Create your views here.
 def Index(request):
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(Index)
+    else:
+        form = TaskForm()
+
     tasks_notcompleted = Task.objects.filter(is_done=False).order_by("priority")
     tasks_completed = Task.objects.filter(is_done=True).order_by("priority")
     context = {
         'tasks_notcompleted': tasks_notcompleted,
         'tasks_completed': tasks_completed,
-        'form': TaskForm,
+        'form': form,
     }
     return render(request, 'index.html', context)
 
