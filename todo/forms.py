d92a06f82193664eec64cd607292b4a6c2a29875
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-08 22:44:09 +0330

    styles and labels were edited in forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,26 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Task
 
 
 class TaskForm(ModelForm):
+
     class Meta:
         model = Task
-        fields = ['title', 'description', 'priority']
+        fields = ['title', 'priority', 'description']
+        labels = {
+            'title': 'موضوع',
+            'description': 'توضیحات',
+            'priority': 'اولویت'
+        }
+        widgets = {
+            'description': Textarea(attrs={ 'rows': 5}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['title'].label_classes = ('form-label')
+        self.fields['priority'].widget.attrs.update({'class': 'form-select'})
+        self.fields['priority'].label_classes = ('form-label')
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].label_classes = ('form-label')
