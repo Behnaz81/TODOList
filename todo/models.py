f0049202c23ede1c890ac4d7946c062ff6b38999
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-09 18:52:22 +0330

    Integer choices were edited to support persian options

--- todo/models.py
+++ todo/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Task(models.Model):
     class Priority(models.IntegerChoices):
-        VERY_HIGH = 1
-        HIGH = 2
-        MEDIUM = 3
-        LOW = 4
-        VERY_LOW = 5
+        VERY_HIGH = 1, "خیلی مهم"
+        HIGH = 2, "مهم"
+        MEDIUM = 3, "متوسط"
+        LOW = 4, "کم"
+        VERY_LOW = 5, "خیلی کم"
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300, null=True, blank=True)
