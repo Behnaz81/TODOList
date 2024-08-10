c107d7a3bf8b1c9d8103f6a82dea74954d8ab96c
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-08-10 19:22:30 +0330

    Model tests were created

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Task
+
 
 # Create your tests here.
+class TaskModelTest(TestCase):
+    def setUp(self):
+        Task.objects.create(title="Wash the dishes", priority=1, description="There are too many dishes!")
+
+    def test_task_title(self):
+        task = Task.objects.get(title="Wash the dishes")
+        self.assertEqual(task.title, "Wash the dishes")
+
+    def test_task_priority(self):
+        task = Task.objects.get(title="Wash the dishes")
+        self.assertEqual(task.priority, 1)
+
+    def test_task_description(self):
+        task = Task.objects.get(title="Wash the dishes")
+        self.assertEqual(task.description, "There are too many dishes!")
